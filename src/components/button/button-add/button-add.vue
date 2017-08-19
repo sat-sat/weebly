9d6d497f26041b9bec74a7cf0995367af4b20f34
satoru-y <satoru@roostergrin.com>
2017-08-19 14:35:34 -0700

    feat(button-add): Add basic component markup and styling

---
+++ src/components/button/button-add/button-add.vue
@@ -0,0 +1,7 @@
+<template lang='pug' src='./button-add.pug'></template>
+
+<script>
+export default {
+  props: ['props']
+}
+</script>
--- src/components/container/container.vue
+++ src/components/container/container.vue
@@ -1,1 +1,12 @@
 <template lang='pug' src='./container.pug'></template>
+
+<script>
+// components
+import ButtonAdd from 'components/button/button-add/button-add'
+
+export default {
+  components: {
+    ButtonAdd
+  }
+}
+</script>
