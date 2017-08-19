3842c76feb717f6bac47230f123dc32da5be2f0a
satoru-y <satoru@roostergrin.com>
2017-08-19 15:49:32 -0700

    feat(search): Finish base component markup and styling

--- src/components/page/page.vue
+++ src/components/page/page.vue
@@ -5,12 +5,14 @@
 import ButtonAdd from 'components/button/button-add/button-add'
 import ButtonExport from 'components/button/button-export/button-export'
 import ButtonImport from 'components/button/button-import/button-import'
+import Search from 'components/search/search'
 
 export default {
   components: {
     ButtonAdd,
     ButtonExport,
-    ButtonImport
+    ButtonImport,
+    Search
   }
 }
 </script>
