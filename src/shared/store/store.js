302776fb11e9eda38310784b020e4942c1527837
satoru-y <satoru@roostergrin.com>
2017-08-20 15:42:05 -0700

    feat(page-size): Add pagination and page size functionality

--- src/shared/store/store.js
+++ src/shared/store/store.js
@@ -7,11 +7,24 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
   state: {
     products: data,
-    pageSize: 5
+    pageSize: 5,
+    currentPage: 1
   },
   mutations: {
     pageSize (state, size) {
       state.pageSize = size
+    },
+    currentPage (state, page) {
+      state.currentPage = page
+    },
+    pageUp (state) {
+      state.currentPage += 1
+    },
+    pageDown (state) {
+      state.currentPage -= 1
+    },
+    selectPage (state, page) {
+      state.currentPage = Number(page)
     }
   }
 })
