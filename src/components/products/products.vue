31df5c7f46c1cd399a342ca6dd9228e5983615a9
satoru-y <satoru@roostergrin.com>
2017-08-19 18:15:31 -0700

    feat(page-size): Add base component markup and styling

--- src/components/products/products.vue
+++ src/components/products/products.vue
@@ -2,11 +2,13 @@
 
 <script>
 // components
+import PageSize from 'components/page-size/page-size'
 import SingleProduct from 'components/single-product/single-product'
 
 export default {
   props: ['props'],
   components: {
+    PageSize,
     SingleProduct
   }
 }
